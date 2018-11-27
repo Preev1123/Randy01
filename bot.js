26755d784811c2d1edea3808eee589e6f07c51af
randycod <45352823+randycod@users.noreply.github.com>
2018-11-27 20:07:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -223,7 +223,6 @@ __ ● ▬▬▬▬▬▬▬▬▬#Randy#▬▬▬▬▬▬▬▬▬ ● __
 ⚙ r.clear ----- ⚙ مسح الشات 
 ⚙ r.mute ----- ⚙ اعطاء ميوت لشخص
 ⚙ r.umute ----- ⚙ فك ميوت من الشخص
-⚙ r.setwelcome ----- ⚙ لترحيب بصوره الامو واختار الروم 
 ⚙ r.avatar ----- ⚙ اظهار صورة للشخص
 
 ⚙ r.id ----- ⚙ اظهار الهوية للشخص
@@ -248,35 +247,7 @@ client.on('message', message => {
     }
 });
 //////////////////////////////////////////////////
-client.on('message', message => {
-    if (message.content.startsWith(prefix + "id")) {
-var args = message.content.split(" ").slice(1);
-let user = message.mentions.users.first();
-var men = message.mentions.users.first();
- var heg;
- if(men) {
-     heg = men
- } else {
-     heg = message.author
- }
-var mentionned = message.mentions.members.first();
-  var h;
- if(mentionned) {
-     h = mentionned
- } else {
-     h = message.member
- }
-        moment.locale('ar-TN');
-var id = new  Discord.RichEmbed()
-.setColor("RANDOM")
-.addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
-.addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
-.addField(": النك نيم",`${h.nickname}`, true) .addField(": #",heg.discriminator, true)
-.addField(`: البلينق`,`${h.presence.game && h.presence.game.name || '-'}`,true) .addField(': الحالة',`${h.presence.status}`,true)
-.addField(`: الرتب`, `${message.guild.members.get(h.id).roles.map(r => `\`${r.name}\``).slice(1).join('\n') || 'لايوجد رتب'}`,true)                                                    
-.setThumbnail(heg.avatarURL);
-message.channel.send(id)
-}       });
+
 //////////////////////////////////////////////////
 client.on('message', message => {
      
