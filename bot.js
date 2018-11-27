c013dfce7b11214585b1bfef6878e212803e9801
randycod <45352823+randycod@users.noreply.github.com>
2018-11-27 15:37:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,18 +25,18 @@ client.on('message', message => {
 });
 /////////////////////////////////////
  client.on("message", (message) => {
-if (message.content.split(' ')[0] == 'obc')
+if (message.content.split(' ')[0] == 'bc')
  message.guild.members.forEach( member => {
          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
                                                             message.delete();
 });
 });
-Client.on("message", message => {
+client.on("message", message => {
     var prefix = "-";
  
             var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix - "bc")) {
+            if (message.content.startsWith(prefix - "obc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
  if (!args[1]) {
                                 let embed3 = new Discord.RichEmbed()
