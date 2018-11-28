9982be73a42212a4b18193134e02b4ce560e8568
randycod <45352823+randycod@users.noreply.github.com>
2018-11-28 13:36:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -230,36 +230,7 @@ function play(guild, song) {
 }
  
 
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '399353508429824000') return;
 
-if (message.content.startsWith(prefix + 'g')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(prefix + 'w')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(prefix + 'l')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(prefix + 's')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-}
- if (message.content.startsWith(adminprefix + 'n')) {
-client.user.setUsername(argresult).then
-       message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else
-}
-});
 client.on("message", message => {
     if (message.content === `${prefix}help`) {
   const embed = new Discord.RichEmbed()
